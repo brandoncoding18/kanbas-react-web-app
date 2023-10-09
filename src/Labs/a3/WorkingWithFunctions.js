47f65308fec69cc2a6aa7da3c05daa16f1ea7ed5
brandoncoding18 <b7ggm8@gmail.com>
2023-10-09 19:53:37 -0400

    JavaScript and React.js Assignment 3

---
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,16 @@
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import ES5Functions from "./ES5Functions";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import JavaScript from "./JavaScript";
+//import ImpliedReturn from "./ImpliedReturn";
+import BooleanVariables from "./BooleanVariables";
+import ArrowFunctions from "./ArrowFunctions";
+function WorkingWithFunctions() {
+ return (
+    <VariableTypes/>
+
+ );
+}
+export default WorkingWithFunctions;
